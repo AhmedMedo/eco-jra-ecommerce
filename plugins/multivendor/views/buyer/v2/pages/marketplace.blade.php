81d2df6433c52770a596e52ffa0ef9888059000c
AhmedMedo <ahmed.alaa.eldin.hamdy@gmail.com>
2025-09-20 23:11:42 +0300

    updates

--- plugins/multivendor/views/buyer/v2/pages/marketplace.blade.php
+++ plugins/multivendor/views/buyer/v2/pages/marketplace.blade.php
@@ -296,10 +296,10 @@
                                 <button class="add-to-cart-btn bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors duration-200 flex items-center" 
                                         data-project-id="{{ $project->id }}"
                                         data-project-name="{{ $project->project_name }}">
-                                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2 8m2-8l2-2m0 0V9a2 2 0 114 0v2.93m-6 0l2 2"/>
-                                    </svg>
-                                    Add to Cart
+{{--                                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+{{--                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2 8m2-8l2-2m0 0V9a2 2 0 114 0v2.93m-6 0l2 2"/>--}}
+{{--                                    </svg>--}}
+                                    Claim
                                 </button>
                             </div>
                         </div>
